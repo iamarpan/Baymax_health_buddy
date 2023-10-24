a4ca4ffd7478c3ed704dbaadac6f5fc8d72ab014
Arpan Srivastava <iamarpan2411@gmail.com>
2023-10-24 17:36:51 +0530

    added logs

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ class ImageProcessor:
 
     def extract_nutrients(self,image_path):
         item_list = self.nutrientExtractor.extract_values(image_path)
+        print("item_list>>>>>",item_list)
         return item_list
 
 
@@ -72,6 +73,7 @@ class ImageProcessor:
             cropped_img = cv2.imread(image_path)[int(y1):int(y2), int(x1):int(x2)]
             output_path = os.path.join('static/cropped', f'cropped_{i}.jpg')
             cv2.imwrite(output_path,cropped_img)
+            print("cls>>>>>>",cls)
             if cls == 1.0:
                 item_list = self.extract_nutrients(output_path)
                         
